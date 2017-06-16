dc113f6c214b7b05a8bd977826c8f8d44a002033
Jan Broeckhove <Jan.Broeckhove@uantwerpen.be>
2017-06-16 16:09:07 +0200

    Addresses .gitignore problems.

---
+++ Makefile
@@ -0,0 +1,127 @@
+#############################################################################
+#  This file is part of the gobelijn software.
+#  Gobelijn is free software: you can redistribute it and/or modify it
+#  under the terms of the GNU General Public License as published by the
+#  Free Software Foundation, either version 3 of the License, or any later
+#  version. Gobelijn is distributed in the hope that it will be useful, but
+#  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+#  or FITNESS FOR A PARTICULAR PURPOSE.
+#  See the GNU General Public License for details. You should have received
+#  a copy of the GNU General Public License along with the software. If not,
+#  see <http://www.gnu.org/licenses/>.
+#
+#  Copyright 2017, Jan Broeckhove, CoMP group, Universiteit Antwerpen.
+#
+#############################################################################
+#
+#	Meta makefile calls cmake to do the heavy lifting. It first
+#	includes the file MakeLocalConfig (if it exists) for local
+#	configuration. If no such file exists the defaults below apply.
+#	This file is tracked by the mercurial repository so do not
+#	change this for personal customization. That should be done in
+#	the file MakeLocalConfig.
+#
+#############################################################################
+
+#============================================================================
+#   Load MakeLocalConfig (if it exists) to override defaults below
+#============================================================================
+MAKEFLAGS   += --no-print-directory
+
+MakeLocalConfig = $(wildcard MakeLocalConfig)
+ifeq ($(MakeLocalConfig),MakeLocalConfig)
+	include MakeLocalConfig
+endif
+
+#============================================================================
+# 	CMake command
+#============================================================================
+ifeq ($(CMAKE),)
+	CMAKE = cmake
+endif
+
+#============================================================================
+#   Platform info: Mac, Linux/UNIX
+#============================================================================
+# Compiling for Mac OSX or *nix
+ifeq ($(CMAKE_GENERATOR),)
+	CMAKE_GENERATOR = "Unix Makefiles"
+endif
+
+#============================================================================
+#   MACRO definitions to pass on to cmake
+#============================================================================
+ifneq ($(CMAKE_GENERATOR),)
+	CMAKE_ARGS += -DCMAKE_GENERATOR=$(CMAKE_GENERATOR)
+endif
+ifneq ($(CMAKE_BUILD_TYPE),)
+	CMAKE_ARGS += -DCMAKE_BUILD_TYPE:STRING=$(CMAKE_BUILD_TYPE)
+endif
+ifneq ($(CMAKE_CXX_COMPILER),)
+	CMAKE_ARGS += -DCMAKE_CXX_COMPILER=$(CMAKE_CXX_COMPILER)
+endif
+ifneq ($(CMAKE_CXX_FLAGS),)
+	CMAKE_ARGS += -DCMAKE_CXX_FLAGS=$(CMAKE_CXX_FLAGS)
+endif
+ifneq ($(CMAKE_INSTALL_PREFIX),)
+	CMAKE_ARGS += -DCMAKE_INSTALL_PREFIX:PATH=$(CMAKE_INSTALL_PREFIX)
+endif
+ifneq ($(GOBELIJN_INCLUDE_DOC),)
+	CMAKE_ARGS += -DGOBELIJN_INCLUDE_DOC:BOOL=$(GOBELIJN_INCLUDE_DOC)
+endif
+ifneq ($(GOBELIJN_VERBOSE_TESTING),)
+	CMAKE_ARGS += -DGOBELIJN_VERBOSE_TESTING:BOOL=$(GOBELIJN_VERBOSE_TESTING)
+endif
+ifeq ($(BUILD_DIR),)
+	BUILD_DIR = build
+endif
+
+#============================================================================
+#   Targets
+#============================================================================
+.PHONY: configure all install install_main install_test  
+.PHONY: clean distclean test installcheck format
+
+help:
+	@ $(CMAKE) -E echo " "
+	@ $(CMAKE) -E echo " Read INSTALL.txt in this directory for a brief overview."
+	@ $(CMAKE) -E echo "    "
+	@ $(CMAKE) -E echo " Current macro values are (cmake will use an appropriate"
+	@ $(CMAKE) -E echo " default for any macro that has not been set):"
+	@ $(CMAKE) -E echo "   BUILD_DIR                 : " $(BUILD_DIR)
+	@ $(CMAKE) -E echo "   CMAKE_GENERATOR           : " $(CMAKE_GENERATOR)
+	@ $(CMAKE) -E echo "   CMAKE_BUILD_TYPE          : " $(CMAKE_BUILD_TYPE)
+	@ $(CMAKE) -E echo "   CMAKE_CXX_COMPILER        : " $(CMAKE_CXX_COMPILER)
+	@ $(CMAKE) -E echo "   CMAKE_CXX_FLAGS           : " $(CMAKE_CXX_FLAGS)
+	@ $(CMAKE) -E echo "   CMAKE_INSTALL_PREFIX      : " $(CMAKE_INSTALL_PREFIX)
+	@ $(CMAKE) -E echo "   GOBELIJN_INCLUDE_DOC      : " $(GOBELIJN_INCLUDE_DOC)
+	@ $(CMAKE) -E echo "   GOBELIJN_VERBOSE_TESTING  : " $(GOBELIJN_VERBOSE_TESTING)
+	@ $(CMAKE) -E echo " "
+				
+configure:
+	$(CMAKE) -E make_directory $(BUILD_DIR)
+	$(CMAKE) -E chdir $(BUILD_DIR) $(CMAKE) $(CMAKE_ARGS) ..
+
+all: configure
+	$(MAKE) $(PARALLEL_MAKE) -C $(BUILD_DIR) all
+
+install: configure
+	$(MAKE) $(PARALLEL_MAKE) -C $(BUILD_DIR) --no-print-directory install   
+	
+install_main:
+	$(MAKE) $(PARALLEL_MAKE) -C $(BUILD_DIR)/main --no-print-directory install
+	
+install_test: install_main
+	$(MAKE) $(PARALLEL_MAKE) -C $(BUILD_DIR)/test --no-print-directory install
+
+distclean clean:
+	$(CMAKE) -E remove_directory $(BUILD_DIR)
+	$(CMAKE) -E remove_directory cmake-build-debug
+
+test installcheck: install_test
+	$(MAKE) $(PARALLEL_MAKE) -C $(BUILD_DIR) run_ctest
+	
+format:
+	resources/bash/clang-format-all .
+	
+#############################################################################
---
+++ main/resources/lib/g3log/Build.cmake
@@ -0,0 +1,139 @@
+# g3log is a KjellKod Logger
+# 2015 @author Kjell Hedström, hedstrom@kjellkod.cc
+# ==================================================================
+# 2015 by KjellKod.cc. This is PUBLIC DOMAIN to use at your own
+#    risk and comes  with no warranties.
+#
+# This code is yours to share, use and modify with no strings attached
+#   and no restrictions or obligations.
+# ===================================================================
+
+
+SET(LOG_SRC ${g3log_SOURCE_DIR}/src)
+include_directories(${LOG_SRC})
+SET(ACTIVE_CPP0xx_DIR "Release")
+
+#cmake -DCMAKE_CXX_COMPILER=clang++ ..
+  # WARNING: If Clang for Linux does not work with full C++11 support it might be your
+  # installation that is faulty. When I tested Clang on Ubuntu I followed the following
+  # description
+  #  1) http://kjellkod.wordpress.com/2013/09/23/experimental-g3log-with-clang/
+  #  2) https://github.com/maidsafe/MaidSafe/wiki/Hacking-with-Clang-llvm-abi-and-llvm-libc
+IF ("${CMAKE_CXX_COMPILER_ID}" MATCHES ".*Clang")
+   MESSAGE("")
+   MESSAGE("cmake for Clang ")
+   SET(CMAKE_CXX_FLAGS "-Wall -std=c++11 -Wno-braced-scalar-init -D_GLIBCXX_USE_NANOSLEEP")
+   IF (${CMAKE_SYSTEM_NAME} STREQUAL "Linux")
+       SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libstdc++ -pthread")
+   ELSE()
+       SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++")
+   ENDIF()
+   IF (${CMAKE_SYSTEM} MATCHES "FreeBSD-([0-9]*)\\.(.*)")
+       IF (${CMAKE_MATCH_1} GREATER 9)
+           set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -pthread")
+           set(PLATFORM_LINK_LIBRIES execinfo)
+       ENDIF()
+   ELSEIF (APPLE)
+       #set(PLATFORM_LINK_LIBRIES c++abi)
+   ELSEIF (NOT (${CMAKE_SYSTEM_NAME} STREQUAL "Linux"))
+       set(PLATFORM_LINK_LIBRIES rt c++abi)
+   ELSE()
+       find_library(RT_LIBRARY rt HINTS /usr/lib/x86_64-linux-gnu /lib/x86_64-linux-gnu)
+       set(PLATFORM_LINK_LIBRIES ${RT_LIBRARY})
+   ENDIF()
+
+ELSEIF("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
+   MESSAGE("cmake for GCC ")
+   IF (APPLE)
+       set(CMAKE_CXX_FLAGS "-Wall -Wunused -std=c++11  -pthread -D_GLIBCXX_USE_NANOSLEEP")
+   ELSEIF (MINGW)
+       set(CMAKE_CXX_FLAGS "-Wall -Wunused -std=c++11  -pthread -D_GLIBCXX_USE_NANOSLEEP -D_GLIBCXX_USE_SCHED_YIELD")
+   ELSE()
+       find_library(RT_LIBRARY rt HINTS /usr/lib/x86_64-linux-gnu /lib/x86_64-linux-gnu)
+       set(PLATFORM_LINK_LIBRIES ${RT_LIBRARY})
+       set(CMAKE_CXX_FLAGS "-Wall -rdynamic -Wunused -std=c++11 -pthread -D_GLIBCXX_USE_NANOSLEEP -D_GLIBCXX_USE_SCHED_YIELD")
+   ENDIF()
+ENDIF()
+
+SET(CMAKE_CXX_FLAGS  "${CMAKE_CXX_FLAGS} -Wformat=0")
+
+IF (MSVC OR MINGW)
+  set(PLATFORM_LINK_LIBRIES dbghelp)
+      # VC11 bug: http://code.google.com/p/googletest/issues/detail?id=408
+      #          add_definition(-D_VARIADIC_MAX=10)
+      # https://github.com/anhstudios/swganh/pull/186/files
+      ADD_DEFINITIONS (/D_VARIADIC_MAX=10)
+      MESSAGE(STATUS "- MSVC: Set variadic max to 10 for MSVC compatibility")
+      # Remember to set set target properties if using GTEST similar to done below on target "unit_test"
+      # "set_target_properties(unit_test  PROPERTIES COMPILE_DEFINITIONS "GTEST_USE_OWN_TR1_TUPLE=0")
+   MESSAGE("")
+   MESSAGE("Windows: Run cmake with the appropriate Visual Studio generator")
+   MESSAGE("The generator is one number below the official version number. I.e. VS2013 -> Generator 'Visual Studio 12'")
+   MESSAGE("I.e. if VS2013: Please run the command [cmake -DCMAKE_BUILD_TYPE=Release -G \"Visual Studio 12\" ..]")
+   MESSAGE("if cmake finishes OK, do 'msbuild g3log.sln /p:Configuration=Release'")
+   MESSAGE("then run 'Release\\g3log-FATAL-*' examples")
+   MESSAGE("")
+ENDIF()
+
+    # GENERIC STEPS
+    file(GLOB HEADER_FILES 
+               ${LOG_SRC}/g3log/*.hpp 
+               ${LOG_SRC}/*.hpp
+               )
+
+
+    INSTALL( FILES ${HEADER_FILES}
+                DESTINATION ${INCLUDE_INSTALL_LOCATION}/g3log
+           )
+           
+   file(GLOB SRC_FILES 
+               ${LOG_SRC}/g3log/*.h 
+               ${LOG_SRC}/g3log/*.hpp 
+               ${LOG_SRC}/*.cpp 
+               ${LOG_SRC}/*.ipp
+               )        
+   IF (MSVC OR MINGW)
+         list(REMOVE_ITEM SRC_FILES  ${LOG_SRC}/crashhandler_unix.cpp)
+   ELSE()
+         list(REMOVE_ITEM SRC_FILES  
+               ${LOG_SRC}/crashhandler_windows.cpp 
+               ${LOG_SRC}/g3log/stacktrace_windows.hpp 
+               ${LOG_SRC}/stacktrace_windows.cpp
+               )
+   ENDIF (MSVC OR MINGW)
+
+   set(SRC_FILES ${SRC_FILES} ${SRC_PLATFORM_SPECIFIC})
+   
+
+# Create the static g3log library
+    include_directories(${LOG_SRC})
+    add_library(g3logger-static STATIC ${SRC_FILES})
+    set_target_properties(g3logger-static PROPERTIES OUTPUT_NAME g3logger )
+    # needed because PLATFORM_LINK_LIBRIES may be blank
+    string(STRIP "-pthread ${PLATFORM_LINK_LIBRIES}" LINK_LIBRARIES)  
+    target_link_libraries(g3logger-static ${LINK_LIBRARIES} )
+    INSTALL( TARGETS g3logger-static ARCHIVE DESTINATION ${LIB_INSTALL_LOCATION} )
+    SET(G3LOG_LIBRARY g3logger-static)
+    
+# Create the shared g3log library
+if(ADD_BUILD_WIN_SHARED OR NOT(MSVC OR MINGW))
+
+        IF(NOT(CMAKE_VERSION LESS 3.4) AND MSVC)
+            set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS ON)
+        ENDIF()
+        add_library(g3logger_shared SHARED ${SRC_FILES})
+        target_link_libraries(g3logger_shared ${PLATFORM_LINK_LIBRIES})
+        set_target_properties(g3logger_shared PROPERTIES OUTPUT_NAME g3logger )
+
+        IF(APPLE)
+            set_target_properties(g3logger_shared PROPERTIES
+                                        MACOSX_RPATH TRUE
+                                        INSTALL_NAME_DIR "${CMAKE_INSTALL_PREFIX}/${LIB_INSTALL_LOCATION}")
+        ENDIF(APPLE)
+        INSTALL( TARGETS g3logger_shared
+                    ARCHIVE DESTINATION ${LIB_INSTALL_LOCATION}
+                    LIBRARY DESTINATION ${LIB_INSTALL_LOCATION}
+        )
+        SET(G3LOG_SHARED_LIBRARY g3logger_shared)
+        
+endif()
