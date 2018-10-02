2610b9ad735e45b58989f1c6ed26b1ac1ff8e8bc
Jan Broeckhove <Jan.Broeckhove@uantwerpen.be>
2018-10-02 10:50:51 +0200

    Removed references to STRIDE macros.

--- resources/cmake/CMakeLocal.cmake
+++ resources/cmake/CMakeLocal.cmake
@@ -25,13 +25,12 @@ message(STATUS "\nReading configuration info from CMakeLocal.cmake! \n")
 #============================================================================
 # MACRO (WARNING: CLion overrides the setting of CMAKE_BUILD_TYPE)
 #============================================================================
-set(STRIDE_INCLUDE_DOC      FALSE)
-set(STRIDE_FORCE_NO_OPENMP  FALSE)
+set(GOBELIJN_INCLUDE_DOC      FALSE)
 
 if(NOT DEFINED CMAKE_CXX_COMPILER)
-    set(STRIDE_COMPILER_ID      GNU)
-    #set(STRIDE_COMPILER_ID      Clang)
-    #set(STRIDE_COMPILER_ID      Apple)
+    set(GOBELIJN_COMPILER_ID      GNU)
+    #set(GOBELIJN_COMPILER_ID      Clang)
+    #set(GOBELIJN_COMPILER_ID      Apple)
 endif()
 if(NOT DEFINED CMAKE_BUILD_TYPE)
     #set(CMAKE_BUILD_TYPE "Debug")
@@ -50,30 +49,30 @@ set(Python_ADDITIONAL_VERSIONS 3.4)
 # Install dir.
 #============================================================================
 execute_process(COMMAND git rev-list HEAD --count
-    OUTPUT_VARIABLE STRIDE_GIT_LABEL OUTPUT_STRIP_TRAILING_WHITESPACE)
-set(CMAKE_INSTALL_PREFIX  $ENV{HOME}/opt/stride-${STRIDE_GIT_LABEL})
+    OUTPUT_VARIABLE GOBELIJN_GIT_LABEL OUTPUT_STRIP_TRAILING_WHITESPACE)
+set(CMAKE_INSTALL_PREFIX  $ENV{HOME}/opt/stride-${GOBELIJN_GIT_LABEL})
 
 #============================================================================
 # Compiler & Boost.
 #============================================================================
 if(LINUX)
-    if(STRIDE_COMPILER_ID STREQUAL "GNU")
+    if(GOBELIJN_COMPILER_ID STREQUAL "GNU")
         set(CMAKE_C_COMPILER   /usr/bin/gcc  CACHE PATH "C compiler path")
         set(CMAKE_CXX_COMPILER /usr/bin/g++  CACHE PATH "CXX compiler path")
-    elseif(STRIDE_COMPILER_ID STREQUAL "Clang")
+    elseif(GOBELIJN_COMPILER_ID STREQUAL "Clang")
         set(CMAKE_C_COMPILER   /usr/bin/clang    CACHE PATH "C compiler path")
         set(CMAKE_CXX_COMPILER /usr/bin/clang++  CACHE PATH "CXX compiler path")
     endif()
 endif()
 #
 if(APPLE)
-    if(STRIDE_COMPILER_ID STREQUAL "GNU")
+    if(GOBELIJN_COMPILER_ID STREQUAL "GNU")
         set(CMAKE_C_COMPILER   /opt/local/bin/gcc  CACHE PATH "C compiler path")
         set(CMAKE_CXX_COMPILER /opt/local/bin/g++  CACHE PATH "CXX compiler path")
-    elseif(STRIDE_COMPILER_ID STREQUAL "Clang")
+    elseif(GOBELIJN_COMPILER_ID STREQUAL "Clang")
         set(CMAKE_C_COMPILER   /opt/local/bin/clang   CACHE PATH "C compiler path")
         set(CMAKE_CXX_COMPILER /opt/local/bin/clang++ CACHE PATH "CXX compiler path")
-    elseif(STRIDE_COMPILER_ID STREQUAL "Apple")
+    elseif(GOBELIJN_COMPILER_ID STREQUAL "Apple")
         set(CMAKE_C_COMPILER   /usr/bin/cc   CACHE PATH "C compiler path")
         set(CMAKE_CXX_COMPILER /usr/bin/c++  CACHE PATH "CXX compiler path")
     endif()
@@ -83,12 +82,12 @@ endif()
 # Compiler & Boost.
 #============================================================================
 if(LINUX)
-    set(STRIDE_BOOST_ROOT "/opt/boost/gcc/boost_1_66_0/")
-    set(STRIDE_BOOST_NO_SYSTEM_PATHS ON)
+    set(GOBELIJN_BOOST_ROOT "/opt/boost/gcc/boost_1_66_0/")
+    set(GOBELIJN_BOOST_NO_SYSTEM_PATHS ON)
 endif()
 #
 if(APPLE)
-    if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR STRIDE_COMPILER_ID STREQUAL "GNU")
+    if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR GOBELIJN_COMPILER_ID STREQUAL "GNU")
         set(BOOST_ROOT /opt/boost-1.67.0)
         set(BOOST_NO_SYSTEM_PATHS ON)
     else()
